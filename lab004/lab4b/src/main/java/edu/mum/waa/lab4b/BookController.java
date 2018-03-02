9f2e874b9902c2e772a05b0319a368028d33ff92
natanael <natanaeltlemos@gmail.com>
2018-03-01 20:56:53 -0600

    Lab4 part2 done

---
+++ lab004/lab4b/src/main/java/edu/mum/waa/lab4b/BookController.java
@@ -0,0 +1,68 @@
+package edu.mum.waa.lab4b;
+
+import javax.validation.*;
+import org.springframework.validation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/bookStore")
+public class BookController {
+
+	@Autowired
+	private IBookDao bookDao;
+	
+	@RequestMapping(value = "/books", method = RequestMethod.GET)
+	public String getAll(Model model) {
+		model.addAttribute("books", bookDao.getAll());
+		return "bookList";
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String add(@ModelAttribute("book") Book book) {
+		return "addBook";
+	}
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String add(@Valid Book book, BindingResult result) {
+		if (result.hasErrors()) {
+			return "addBook";
+		}
+
+		bookDao.add(book);
+		return "redirect:/bookStore/books";
+	}
+
+	@RequestMapping(value = "/books/{id}", method = RequestMethod.GET)
+	public String get(@ModelAttribute("bookModel") Book bookModel, @PathVariable int id, Model model) {
+		model.addAttribute("book", bookDao.get(id));
+		return "bookDetail";
+	}
+
+	@RequestMapping(value = "/books/{id}", method = RequestMethod.POST)
+	public String update(Book book, @PathVariable int id, BindingResult result) {
+		if (result.hasErrors()) {
+			return "bookDetail";
+		}
+		
+		bookDao.update(id, book);
+		return "redirect:/bookStore/books";
+	}
+
+	@RequestMapping(value = "/books/delete", method = RequestMethod.POST)
+	public String delete(int bookId) {
+		bookDao.delete(bookId);
+		return "redirect:/bookStore/books";
+	}
+
+	@ExceptionHandler(value = NoSuchResourceException.class)
+	public ModelAndView handle(Exception e) {
+		ModelAndView mv = new ModelAndView();
+		mv.getModel().put("e", e);
+		mv.setViewName("noSuchResource");
+		return mv;
+	}
+}
---
+++ lab004/lab4b/src/main/java/edu/mum/waa/lab4b/Lab4bApplication.java
@@ -0,0 +1,12 @@
+package edu.mum.waa.lab4b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4bApplication.class, args);
+	}
+}
