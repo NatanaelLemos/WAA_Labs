52ddf713359d14b0df8ee740dddebf7f3a8eee42
unknown <986242@cs.mum.edu>
2018-03-03 11:56:34 -0600

    Attempt to use security 2

--- lab005/exercise8_5/src/main/java/edu/mum/waa/exercise8_5/SecurityWebApplicationlnitializer.java
+++ lab005/exercise8_5/src/main/java/edu/mum/waa/exercise8_5/SecurityWebApplicationlnitializer.java
@@ -1,9 +1,9 @@
-//package edu.mum.waa.exercise8_5;
-//
-//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-//
-//public class SecurityWebApplicationlnitializer extends AbstractSecurityWebApplicationInitializer {
-//	public SecurityWebApplicationlnitializer() {
-//		super(WebSecurityConfig.class);
-//	}
-//}
+package edu.mum.waa.exercise8_5;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationlnitializer extends AbstractSecurityWebApplicationInitializer {
+	public SecurityWebApplicationlnitializer() {
+		super(WebSecurityConfig.class);
+	}
+}
--- lab005/exercise8_5/src/main/java/edu/mum/waa/exercise8_5/WebSecurityConfig.java
+++ lab005/exercise8_5/src/main/java/edu/mum/waa/exercise8_5/WebSecurityConfig.java
@@ -1,31 +1,29 @@
-//package edu.mum.waa.exercise8_5;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@EnableWebSecurity
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//	@Autowired
-//	public void yourAuthenticationConfig(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.inMemoryAuthentication()
-//		.withUser("sales").password("123").roles("SALES")
-//		.and()
-//		.withUser("admin").password("123").roles("ADMIN")
-//		.and()
-//		.withUser("books").password("123").roles("ACCOUNT");
-//	}
-//	
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests()
-//		.antMatchers("/login", "/login-error").permitAll() 
-//		.antMatchers("/cars").hasRole("SALES")
-//		.antMatchers("/bookStore").hasRole("ACCOUNT") 
-//		.and() 
-//		.formLogin().loginPage("/login").failureUrl("/login-error").successForwardUrl("/").permitAll(); 
-//	}
-//}
-//
+package edu.mum.waa.exercise8_5;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	public void yourAuthenticationConfig(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("sales").password("123").roles("SALES");
+		auth.inMemoryAuthentication().withUser("admin").password("123").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("books").password("123").roles("ACCOUNT");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/login", "/login-error", "/").permitAll()
+			.antMatchers("/cars").hasRole("SALES")
+			.antMatchers("/bookStore").hasRole("ACCOUNT")
+			.and().formLogin().permitAll();
+	}
+}
