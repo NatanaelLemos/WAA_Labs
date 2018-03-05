aeeefe05b070fae338ab42ea7175d6abf2275cfb
natanael <natanaeltlemos@gmail.com>
2018-03-04 22:19:24 -0600

    Lab006 done

---
+++ lab006/src/main/java/edu/mum/lab006/Lab006Application.java
@@ -0,0 +1,12 @@
+package edu.mum.lab006;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab006Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab006Application.class, args);
+	}
+}
